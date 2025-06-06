0acdd48a93cba427b40f9a209d8bdadf41a730a6
Thomas Doherty <dohert7@rpi.edu>
2025-06-06 14:41:21 -0400

    Fixed the program, works now but the gifs dont load when the bot sends them

--- src/capy_app/frontend/cogs/onboarding/thomas_onboarding_cog.py
+++ src/capy_app/frontend/cogs/onboarding/thomas_onboarding_cog.py
@@ -16,33 +16,33 @@ class ThomasCog(commands.Cog):
         )
         
         self.random_lines = [
-            "Every good thing that happens to you in this game is preplanned like a show. There's an audience waiting for your downfall."
-            "...I should buy a boat."
-            "So... come here often?"
-            "Wanna hear a joke?"
-            "Im out of funny ideas, come back later"
-            "Check out this cool gif"
-            "I'm hungry. Like, I really could go for some rigatoni right now."
+            "Every good thing that happens to you in this game is preplanned like a show. There's an audience waiting for your downfall.",
+            "...I should buy a boat.",
+            "So... come here often?",
+            "Wanna hear a joke?",
+            "Im out of funny ideas, come back later",
+            "Check out this cool, funny, humerous, and completely original gif",
+            "I'm hungry. Like, I really could go for some rigatoni right now.",
             "I've been thinking of starting a band recently. Might call it [adjective] [noun]. What do you think?"
         ]
         
         self.random_images = [
-            "https://tenor.com/view/destroyman-destroyman-iii-destroyman-3-deepwoken-deepwoken-destroyman-gif-7822491813183790485"
-            "https://tenor.com/view/destroyman-destroyman-iii-destroyman-3-deepwoken-deepwoken-destroyman-gif-241781888877782845"
-            "https://media.discordapp.net/attachments/820192358158172160/1099738116329320598/EF5CD357-44D9-4097-8F3F-E67BF676FEA2.gif?ex=6840786f&is=683f26ef&hm=b2436104426390d1b0d2bbef8b5ac9a9e6db345e21a13a2059787a0db5a250d0&"
-            "https://tenor.com/view/gullible-gif-23847583"
-            "https://tenor.com/view/petting-aligator-crocodile-gif-25954785"
+            "https://tenor.com/view/destroyman-destroyman-iii-destroyman-3-deepwoken-deepwoken-destroyman-gif-7822491813183790485",
+            "https://tenor.com/view/destroyman-destroyman-iii-destroyman-3-deepwoken-deepwoken-destroyman-gif-241781888877782845",
+            "https://media.discordapp.net/attachments/820192358158172160/1099738116329320598/EF5CD357-44D9-4097-8F3F-E67BF676FEA2.gif?ex=6840786f&is=683f26ef&hm=b2436104426390d1b0d2bbef8b5ac9a9e6db345e21a13a2059787a0db5a250d0&",
+            "https://tenor.com/view/gullible-gif-23847583",
+            "https://tenor.com/view/petting-aligator-crocodile-gif-25954785",
             "https://tenor.com/view/obama-ballin-basketball-gif-19387821"
         ]
 
     @app_commands.guilds(discord.Object(id=settings.DEBUG_GUILD_ID))
-    @app_commands.command(name="thomas", description="Does Something")
+    @app_commands.command(name="ifunny", description="Does Something Cool and stuff idk")
     async def Thomas(self, interaction: discord.Interaction):
         line = random.choice(self.random_lines)
         image = random.choice(self.random_images)
         
         embed = discord.Embed(
-            title="Tom",
+            title="Im So Funny!!!",
             description=line,
             color=colors.PING,
         )
